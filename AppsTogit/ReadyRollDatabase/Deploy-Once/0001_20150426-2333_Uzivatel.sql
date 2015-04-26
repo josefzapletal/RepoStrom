9255ea7d6d16acdcc0afaca0f2d41a821765888c
josefzapletal <josef.zapletal@hotmail.com>
2015-04-26 23:37:16 +0200

    ReadyRoll, Release 1.0

---
+++ AppsTogit/ReadyRollDatabase/Deploy-Once/0001_20150426-2333_Uzivatel.sql
@@ -0,0 +1,23 @@
+﻿-- <Migration ID="6af0c1c7-1794-4db9-bea8-27bc9905d5e5" />
+GO
+
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+
+GO
+
+
+Print 'Create Table [dbo].[Planet]'
+GO
+CREATE TABLE [dbo].[Planet] (
+		[Id]             [int] NULL,
+		[PlanetName]     [nvarchar](100) NULL
+)
+GO
+
+
+ALTER TABLE [dbo].[Planet] SET (LOCK_ESCALATION = TABLE)
+GO
+
+
---
+++ AppsTogit/ReadyRollDatabase/Deploy-Once/0002_20150426-2334_Uzivatel.sql
@@ -0,0 +1,17 @@
+﻿-- <Migration ID="7290d629-63e8-4d11-85a7-c724d562c953" />
+GO
+
+IF (SELECT COUNT(*)
+    FROM   [dbo].[Planet]) = 0
+    BEGIN
+        PRINT 'Synchronization Script for Table: [dbo].[Planet]';
+        INSERT  INTO [dbo].[Planet] ([Id], [PlanetName])
+        VALUES                     (NULL, N'Neptun');
+        INSERT  INTO [dbo].[Planet] ([Id], [PlanetName])
+        VALUES                     (NULL, N'Pluto');
+        INSERT  INTO [dbo].[Planet] ([Id], [PlanetName])
+        VALUES                     (NULL, N'Uran');
+    END
+
+
+GO
